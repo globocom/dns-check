21a367937f06cd370f04acfb2499ad53af9b75e0
nelson.marcos <nelson.marcos@corp.globo.com>
2021-12-24 00:37:23 -0300

    Deleting unsed files

--- test.go
+++
@@ -1,8 +0,0 @@
-attended := map[string]bool{
-    "Ann": true,
-    "Joe": true,
-}
-
-if attended[person] { // will be false if person is not in the map
-    fmt.Println(person, "was at the meeting")
-}
