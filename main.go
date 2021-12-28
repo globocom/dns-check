9d244d225b7ec7fca2962ea7aa56ec1be3e54e78
nelson.marcos <nelson.marcos@corp.globo.com>
2021-12-28 09:20:41 -0300

    Improving flags help

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
- 
+
 import (
 	"context"
 	"flag"
 	"fmt"
 	"net"
 	"sort"
-    "strings"
+	"strings"
 	"sync"
-
 )
 
 //Global varialbes
@@ -17,8 +16,7 @@ var wg sync.WaitGroup
 var mutex = &sync.RWMutex{}
 var DefaultResolver = net.Resolver{PreferGo: true}
 
-
-// printProgressBar prints a friendly progress bar so the user can be sure if the program is running or waiting 
+// printProgressBar prints a friendly progress bar so the user can be sure if the program is running or waiting
 func printProgressBar(iteration, total int, prefix, suffix string, length int, fill string) {
 	percent := float64(iteration) / float64(total) * 100
 	filledLength := int(length * iteration / total)
@@ -36,57 +34,56 @@ func printProgressBar(iteration, total int, prefix, suffix string, length int, f
 
 //LookupIP is a replacemente for default LookupIP so we can use go as dns resolver instead of the OS
 func LookupIP(host string) ([]net.IP, error) {
-    addrs, err := DefaultResolver.LookupIPAddr(context.Background(), host)
-    if err != nil {
-        return nil, err
-    }
+	addrs, err := DefaultResolver.LookupIPAddr(context.Background(), host)
+	if err != nil {
+		return nil, err
+	}
 	ips := make([]net.IP, len(addrs))
-    for i, ia := range addrs {
-        ips[i] = ia.IP
-    }
-    return ips, nil
+	for i, ia := range addrs {
+		ips[i] = ia.IP
+	}
+	return ips, nil
 }
-
 
 // dnsResolver resolves a dns add the result to a map and has concurrency control
-func dnsResolver(domain string){
-		wg.Add(1)
-		iprecords, _ := LookupIP(domain)
-		for _, ip := range iprecords {
-			mutex.Lock()
-        	result[ip.String()]++
-			mutex.Unlock()
-		}
+func dnsResolver(domain string) {
+	wg.Add(1)
+	iprecords, _ := LookupIP(domain)
+	for _, ip := range iprecords {
+		mutex.Lock()
+		result[ip.String()]++
+		mutex.Unlock()
+	}
 }
 func main() {
-	domainPtr := flag.String("domain", "","Domain to be resolved")
-	repeatPtr := flag.Int("r", 1,"Number of run times")
-	multithread := flag.Bool("d", false, "Enables multithread")
-	var setWait bool 
+	domainPtr := flag.String("domain", "", "`Domain` to be resolved")
+	repeatPtr := flag.Int("r", 1, "`Number` of run times")
+	multithread := flag.Bool("d", false, "Enables multithread. Default is false.")
+	var setWait bool
 	flag.Parse()
-    fmt.Println("domain:", *domainPtr)
-	for rep := 1; rep <= *repeatPtr ; rep++ {
+	fmt.Println("domain:", *domainPtr)
+	for rep := 1; rep <= *repeatPtr; rep++ {
 		printProgressBar(rep, *repeatPtr, "Progress", "Complete", 25, "=")
 		if *multithread {
-		    setWait = true
-		    go func() {
-                 defer wg.Done()
-                 dnsResolver(*domainPtr)
-            }()
+			setWait = true
+			go func() {
+				defer wg.Done()
+				dnsResolver(*domainPtr)
+			}()
 		} else {
-		    dnsResolver(*domainPtr)
+			dnsResolver(*domainPtr)
 		}
-   }
-   if setWait == true {
-		   wg.Wait()
-   }
-   allkeys := make([]string, 0, len(result))
-   for key := range result {
-       allkeys = append(allkeys, key)
-   }
-    sort.Strings(allkeys)
+	}
+	if setWait == true {
+		wg.Wait()
+	}
+	allkeys := make([]string, 0, len(result))
+	for key := range result {
+		allkeys = append(allkeys, key)
+	}
+	sort.Strings(allkeys)
 
 	for _, key := range allkeys {
-        fmt.Println(key,"=",result[key])
-    }
+		fmt.Println(key, "=", result[key])
+	}
 }
